6adbdfbebfc056bcbb49d2c0c2f281fa693d281b
José María Nevado Contreras <A183599@AT4WIRELESS>
2023-04-25 12:01:35 +0200

    Funcion suma y resta

--- Funcion sumar/Proyecto_Git_1_función_sumar.py
+++
@@ -1,7 +0,0 @@
-num1=4
-num2=3
-
-def sumar(n1,n2):
-    return n1+n2
-
-print(sumar(num1,num2))
---
+++ Funciones matematicas/Proyecto_Git_1_funciones_matematicas.py
@@ -0,0 +1,11 @@
+num1=4
+num2=3
+
+def sumar(n1,n2):
+    return n1+n2
+
+def restar(n1,n2):
+    return n1-n2
+
+print(sumar(num1,num2))
+print(restar(num1,num2))
