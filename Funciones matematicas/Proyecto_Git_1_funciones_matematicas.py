cc317a82d1762c274f5a6e696eb90844b8cf17f5
joseenev <131764882+joseenev@users.noreply.github.com>
2023-04-25 13:37:48 +0200

    Update Proyecto_Git_1_funciones_matematicas.py

--- Funciones matematicas/Proyecto_Git_1_funciones_matematicas.py
+++ Funciones matematicas/Proyecto_Git_1_funciones_matematicas.py
@@ -7,5 +7,9 @@ def sumar(n1,n2):
 def restar(n1,n2):
     return n1-n2
 
+def multiplicar(n1,n2):
+    return n1*n2
+
 print(sumar(num1,num2))
 print(restar(num1,num2))
+print(multiplicar(num1,num2))
